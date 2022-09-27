4c0f818c51f3ca9406c134b82c04ff7caec990f4
Derrick Chambers <d-chambers@users.noreply.github.com>
2022-09-26 20:16:39 -0600

    add docstring

--- myscript.py
+++ myscript.py
@@ -1,5 +1,10 @@
+"""
+A script to practice using functions.
+"""
 def add_numbers(num1, num2):
     """Add two things together"""
     return num1 + num2
 
-assert add_numbers(1, 1) == 2
+
+if __name__ == "__main__":
+    assert add_numbers(1, 1) == 2
